3ceee37c0d561eb77f8f052b55e46e841801d962
SixArm <sixarm@sixarm.com>
2012-03-16 10:08:52 -0700

    Tune up organization

--- test/sixarm_ruby_sign_in_simple_test.rb
+++ test/sixarm_ruby_sign_in_simple_test.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+require 'minitest/autorun'
 require 'simplecov'
 SimpleCov.start
-require 'minitest/autorun'
 require 'sixarm_ruby_sign_in_simple'
 require 'sixarm_ruby_application_controller_mock'
 
